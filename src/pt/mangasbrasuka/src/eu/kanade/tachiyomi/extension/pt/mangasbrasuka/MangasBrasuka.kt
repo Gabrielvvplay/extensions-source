adba74983373c514290d6d19aa9303283253fbd8
Nerdolas <90934934+Gabrielvvplay@users.noreply.github.com>
2026-09-16 08:54:35 -0300

    Refactor MangasBrasuka class implementation
    mudei pra um novo padrao pra acharr os capitulos

--- src/pt/mangasbrasuka/src/eu/kanade/tachiyomi/extension/pt/mangasbrasuka/MangasBrasuka.kt
+++ src/pt/mangasbrasuka/src/eu/kanade/tachiyomi/extension/pt/mangasbrasuka/MangasBrasuka.kt
@@ -2,6 +2,21 @@ package eu.kanade.tachiyomi.extension.pt.mangasbrasuka
 
 import eu.kanade.tachiyomi.multisrc.aurora.Aurora
 import keiyoushi.annotation.Source
+import org.jsoup.nodes.Element
 
 @Source
-abstract class MangasBrasuka : Aurora()
+class MangasBrasuka : Aurora(
+    "Mangas Brasuka",
+    "https://mangasbrasuka.org",
+    "pt-BR",
+) {
+    override fun chapterListSelector() = "ul li:has(a[href*='/manga/'])"
+
+    override fun chapterFromElement(element: Element) = super.chapterFromElement(element).apply {
+        val link = element.selectFirst("a[href*='/manga/']")
+        if (link != null) {
+            setUrlWithoutDomain(link.attr("href"))
+            name = link.selectFirst("span, p, div")?.text() ?: link.text()
+        }
+    }
+}
