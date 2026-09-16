a70e351aaf516d9cc4f7470794a336ec22afecd7
Nerdolas <90934934+Gabrielvvplay@users.noreply.github.com>
2026-09-16 09:53:08 -0300

    Update MangasBrasuka.kt

--- src/pt/mangasbrasuka/src/eu/kanade/tachiyomi/extension/pt/mangasbrasuka/MangasBrasuka.kt
+++ src/pt/mangasbrasuka/src/eu/kanade/tachiyomi/extension/pt/mangasbrasuka/MangasBrasuka.kt
@@ -5,18 +5,20 @@ import keiyoushi.annotation.Source
 import org.jsoup.nodes.Element
 
 @Source
-class MangasBrasuka : Aurora(
-    "Mangas Brasuka",
-    "https://mangasbrasuka.org",
-    "pt-BR",
-) {
+class MangasBrasuka :
+    Aurora(
+        "Mangas Brasuka",
+        "https://mangasbrasuka.org",
+        "pt-BR",
+    ) {
     override fun chapterListSelector() = "ul li:has(a[href*='/manga/'])"
 
-    override fun chapterFromElement(element: Element) = super.chapterFromElement(element).apply {
-        val link = element.selectFirst("a[href*='/manga/']")
-        if (link != null) {
-            setUrlWithoutDomain(link.attr("href"))
-            name = link.selectFirst("span, p, div")?.text() ?: link.text()
+    override fun chapterFromElement(element: Element) =
+        super.chapterFromElement(element).apply {
+            val link = element.selectFirst("a[href*='/manga/']")
+            if (link != null) {
+                setUrlWithoutDomain(link.attr("href"))
+                name = link.selectFirst("span, p, div")?.text() ?: link.text()
+            }
         }
-    }
 }
