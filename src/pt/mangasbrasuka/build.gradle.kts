90908360c76a9fe7efe5bace75382748420fd6e8
Nerdolas <90934934+Gabrielvvplay@users.noreply.github.com>
2026-09-16 08:45:41 -0300

    Update versionCode from 55 to 59

--- src/pt/mangasbrasuka/build.gradle.kts
+++ src/pt/mangasbrasuka/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 keiyoushi {
     name = "Mangas Brasuka"
-    versionCode = 55
+    versionCode = 59
     contentWarning = ContentWarning.MIXED
     libVersion = "1.6"
     theme = "aurora"
